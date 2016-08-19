36cd1200ec6f61e429a93518bbf289f46324b802
Michal Stuglik <stuglik@gmail.com>
2016-08-19 16:41:59 +0100

    Provision with production flag

--- galaxy_provision.sh
+++ galaxy_provision.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 # provision
-/usr/bin/env ansible-playbook galaxy_provision.yml -i hosts/staging "$@"  -K
+#/usr/bin/env ansible-playbook galaxy_provision.yml -i hosts/staging "$@"  -K
+
+# provision
+/usr/bin/env ansible-playbook galaxy_provision.yml -i hosts/staging "$@"  -e mode=production -K
