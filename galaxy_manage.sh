dcd5595afaeba9282a00f1d468f90355e3f707df
Michal Stuglik <stuglik@gmail.com>
2016-08-19 13:31:47 +0100

    Improve cmd service start/stop/restart, refactored provision/setup playbooks

--- galaxy_manage.sh
+++ galaxy_manage.sh
@@ -1,12 +1,54 @@
 #!/usr/bin/env bash
 
 # start
-/usr/bin/env ansible-playbook galaxy_manage.yml -i hosts/staging "$@" -e state='start'
+function start {
+    /usr/bin/env ansible-playbook galaxy_manage.yml -i hosts/staging "$@" -e state='start'
+}
 
 # stop
-/usr/bin/env ansible-playbook galaxy_manage.yml -i hosts/staging "$@" -e state='stop'
+function stop {
+    /usr/bin/env ansible-playbook galaxy_manage.yml -i hosts/staging "$@" -e state='stop'
+}
 
 # restart
-/usr/bin/env ansible-playbook galaxy_manage.yml -i hosts/staging "$@" -e state='stop'
-/usr/bin/env ansible-playbook galaxy_manage.yml -i hosts/staging "$@" -e state='start'
+function restart {
+    /usr/bin/env ansible-playbook galaxy_manage.yml -i hosts/staging "$@" -e state='stop'
+    /usr/bin/env ansible-playbook galaxy_manage.yml -i hosts/staging "$@" -e state='start'
+}
+
+
+while [ ! $# -eq 0 ]
+do
+    case "$1" in
+        --help | -h)
+            helpmenu
+            exit
+            ;;
+        --start | -a)
+            start
+            exit
+            ;;
+        --stop | -b)
+            stop
+            exit
+            ;;
+        --restart | -c)
+            restart
+            exit
+            ;;
+
+    esac
+    shift
+done
+
+
+# start
+#/usr/bin/env ansible-playbook galaxy_manage.yml -i hosts/staging "$@" -e state='start'
+
+# stop
+#/usr/bin/env ansible-playbook galaxy_manage.yml -i hosts/staging "$@" -e state='stop'
+
+# restart
+#/usr/bin/env ansible-playbook galaxy_manage.yml -i hosts/staging "$@" -e state='stop'
+#/usr/bin/env ansible-playbook galaxy_manage.yml -i hosts/staging "$@" -e state='start'
 
--- galaxy_setup.sh
+++ galaxy_setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # install galaxy instance (lite)
-/usr/bin/env ansible-playbook galaxy_setup.yml -i hosts/staging "$@" -e repo="release_16.04" #-K
+/usr/bin/env ansible-playbook galaxy_setup.yml -i hosts/staging "$@" -e repo="release_16.04" -K
 
 # install galaxy instance
 #/usr/bin/env ansible-playbook galaxy_setup.yml -i hosts/staging "$@" -e repo="release_16.04" -e database_type="postgres" #-K
